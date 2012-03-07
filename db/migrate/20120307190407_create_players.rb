a8e5e77676b0c14f9f9713c56aaabf428ce6ab43
Richard Hurt <rhurt@cafepress.com>
2012-03-07 14:42:45 -0500

    added Player model

---
+++ db/migrate/20120307190407_create_players.rb
@@ -0,0 +1,12 @@
+class CreatePlayers < ActiveRecord::Migration
+  def change
+    create_table :players do |t|
+      t.string  :first_name
+      t.string  :last_name
+      t.boolean :is_boss, :default => false
+
+      t.boolean :active, :default => true
+      t.timestamps
+    end
+  end
+end
