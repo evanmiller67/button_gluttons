2bf044bb2436d374d118ab70ae097f88d2bf62a9
Richard Hurt <rhurt@cafepress.com>
2012-03-07 14:58:25 -0500

    Tweaked the player model

--- app/models/player.rb
+++ app/models/player.rb
@@ -1,2 +1,8 @@
 class Player < ActiveRecord::Base
+
+  default_scope where(:active => true)
+  class << self
+    def active;     where(:active => true); end
+    def registered; where(:is_registered => true); end
+  end
 end
