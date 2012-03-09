f4da1229fb3b6990c1f01810dbdc97a630dd5f8e
Evan Miller <evanmiller67@gmail.com>
2012-03-09 08:31:56 -0500

    seed db with players

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Player.delete_all
+count = 0
+File.open("#{Rails.root}/db/qr_codes.txt", "r") do |infile|
+  infile.each do |code|
+    player = Player.create
+    puts "Updating player: #{code}"
+    Player.connection.execute("UPDATE players SET id = #{code} WHERE id = #{player.id}")
+    count += 1
+  end
+end
+puts "Updated #{count} players"
