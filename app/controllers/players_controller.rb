020932a44608d601b0e17b3af523f42abd1f0ae9
Richard Hurt <rhurt@cafepress.com>
2012-03-12 10:23:16 -0400

    forced Player to only edit themselves

--- app/controllers/players_controller.rb
+++ app/controllers/players_controller.rb
@@ -39,40 +39,14 @@ class PlayersController < ApplicationController
     end
   end
 
-  # GET /players/new
-  # GET /players/new.json
-  def new
-    @player = Player.new
-
-    respond_to do |format|
-      format.html # new.html.erb
-      format.json { render json: @player }
-    end
-  end
-
   # GET /players/1/edit
   def edit
     @player = Player.find(params[:id])
+    
+    redirect_to :root, notice: "You don't own this account" unless @player.is_registered && cookies[:player_id].to_i == @player.id
   end
 
-  # POST /players
-  # POST /players.json
-  def create
-    @player = Player.new(params[:player])
 
-    respond_to do |format|
-      if @player.save
-        format.html { redirect_to @player, notice: 'Player was successfully created.' }
-        format.json { render json: @player, status: :created, location: @player }
-      else
-        format.html { render action: "new" }
-        format.json { render json: @player.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # PUT /players/1
-  # PUT /players/1.json
   def update
     @player = Player.find(params[:id])
     @player.is_registered = true
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 ButtonGluttons::Application.routes.draw do
   match '/players' => redirect('/')
 
-  resources :players
-  resources :fights
+  resources :players, :only => [:show, :update, :edit]
+  resources :fights,  :except => [:destroy, :index, :show]
     
   root :to => 'dashboard#index'
 end
