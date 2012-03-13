93d235d7661278a24101f41bcbe1eefbd755b1c4
Richard Hurt <rhurt@cafepress.com>
2012-03-13 09:51:34 -0400

    converted Player ID to bigint for PostgreSQL

--- db/migrate/20120307190407_create_players.rb
+++ db/migrate/20120307190407_create_players.rb
@@ -1,6 +1,7 @@
 class CreatePlayers < ActiveRecord::Migration
   def change
     create_table :players do |t|
+      t.integer :id, :limit => 8
       t.string  :first_name
       t.string  :last_name
       t.string  :email_address
