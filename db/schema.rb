d785452668f71e2377b37582388bce85c82734f8
Richard Hurt <rhurt@cafepress.com>
2012-03-15 16:14:52 -0400

    rework error pages a bit

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120315141422) do
+ActiveRecord::Schema.define(:version => 20120315195036) do
 
   create_table "fights", :force => true do |t|
     t.integer  "started_by_id"
