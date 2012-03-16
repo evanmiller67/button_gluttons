fd9cbe54db40bcbe9fa24ed25fc6a07155e5069c
Richard Hurt <rhurt@cafepress.com>
2012-03-16 17:23:11 -0400

    fixed reload bug

--- app/assets/javascripts/dashboard.js
+++ app/assets/javascripts/dashboard.js
@@ -1,5 +1,3 @@
-//= require jquery
-//= require jquery_ujs
 //= require twitter/jquery.juitter
 $.Juitter.start({
   searchType:"searchWord", // needed, you can use "searchWord", "fromUser", "toUser"
@@ -9,11 +7,11 @@ $.Juitter.start({
   // They are optional here.
   // I'm changing here as a example only
   lang:"en", // restricts the search by the given language
-  live:"live-30", // the number after "live-" indicates the time in seconds to wait before request the Twitter API for updates.
+  live:"live-60", // the number after "live-" indicates the time in seconds to wait before request the Twitter API for updates.
   placeHolder:"juitterLeaderboard", // Set a place holder DIV which will receive the list of tweets example <div id="juitterContainer"></div>
   loadMSG: "Loading messages...", // Loading message, if you want to show an image, fill it with "image/gif" and go to the next variable to set which image you want to use on 
   imgName: "loader.gif", // Loading image, to enable it, go to the loadMSG var above and change it to "image/gif"
-  total: 6, // number of tweets to be show - max 100
+  total: 7, // number of tweets to be show - max 100
   readMore: " ", // read more message to be show after the tweet content
   nameUser:"image", // insert "image" to show avatar of "text" to show the name of the user that sent the tweet 
   openExternalLinks:"newWindow", // here you can choose how to open link to external websites, "newWindow" or "sameWindow"
--- app/assets/javascripts/fights.js.coffee
+++ app/assets/javascripts/fights.js.coffee
@@ -3,16 +3,19 @@
 # You can use CoffeeScript in this file: http://jashkenas.github.com/coffee-script/
 
 showscore = ->
-  $('.score.player').removeClass('hidden')
-  $('.weapon.player').attr('src', $('.player.weapon.cache').attr('src'))
+  if $('#winlose').html() != null
+    $('.score.player').removeClass('hidden')
+    $('.weapon.player').attr('src', $('.player.weapon.cache').attr('src'))
 
 showwinner = ->
-  unless $('.winlose.graphic').data("status") == ""
-    $('#winlose').slideDown();
+  if $('#winlose').html() != null
+    unless $('.winlose.graphic').data("status") == ""
+      $('#winlose').slideDown();
 
 redirectpage = ->
-  window.location = "http://buttongluttons.com/leaderboard"
-  return false
+  if $('#winlose').html() != null
+   window.location = "http://buttongluttons.com/leaderboard"
+   return false
 
 setTimeout showscore, 3000
 setTimeout showwinner, 3500
